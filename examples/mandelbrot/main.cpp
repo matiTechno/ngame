3415e5b5d900db081430a26c87654744a4daf7b3
Mateusz <mateusz.macieja8@gmail.com>
2017-06-12 16:28:56 +0200

    Create main.cpp
    changed gl version to 3.3

--- examples/mandelbrot/main.cpp
+++ examples/mandelbrot/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     try
     {
-        App app(800, 600, "mandelbrot", true, 4, 3, SDL_WINDOW_FULLSCREEN_DESKTOP);
+        App app(800, 600, "mandelbrot", true, 3, 3, SDL_WINDOW_FULLSCREEN_DESKTOP);
         app.start<Mandelbrot>();
     }
     catch(const std::exception& e)
